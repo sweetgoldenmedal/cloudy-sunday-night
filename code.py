05f62b5a09c8051968b8fef27b0cf3685321b38e
sweetgoldenmedal <sweetgoldenmedal@gmail.com>
2019-10-15 14:25:10 -0700

    putting ns code back in

--- code.py
+++ code.py
@@ -8,22 +8,13 @@ import time
 #    cpx.red_led = False
 #    time.sleep(.01)
 
-cpx.red_led= False
+cpx.red_led = False
 
 while True:
-  for i in range(10):
-    if i % 2 == 0:
-      cpx.pixels[i] = [0,1,0]
-    else:
-      cpx.pixels[i] = [1,0,0]
-    time.sleep(1)
-        #cpx.pixels[(i-9)%10] = (0,0,0)
-        #cpx.pixels[(i-6)%10] = (20,20,20)
-        #cpx.pixels[(i-3)%10] = (0,0,20)
-        #cpx.pixels[i] = (20,0,0)
-        #cpx.pixels[(i+1)%10] = (20,0,0)
-        #time.sleep(.1)
+  cpx.pixels[(i-9)%10] = (0,0,0)
+  cpx.pixels[(i-6)%10] = (20,20,20)
+  cpx.pixels[(i-3)%10] = (0,0,20)
+  cpx.pixels[i] = (20,0,0)
+  cpx.pixels[(i+1)%10] = (20,0,0)
+  time.sleep(.1)
 
-    #for i in range(10):
-    #    cpx.pixels[i] = [0,0,0]
-    #    time.sleep(1)
