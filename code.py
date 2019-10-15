12259443d7508a7f00ce85c1512ee73ed17ed019
Martin Gonzalez <mgonzo@gmail.com>
2019-10-15 15:12:54 -0700

    Added a flasher and reset at the end... maybe.

--- code.py
+++ code.py
@@ -1,19 +1,26 @@
 # The cpx will execute the contents of this file
 
 # see https://learn.adafruit.com/circuitpython-made-easy-on-circuit-playground-express/neopixels
-# for additional cpx functionality 
+# for additional cpx functionality
 
 from adafruit_circuitplayground.express import cpx
 import time
 
-cpx.red_led = True 
+cpx.red_led = True
 
 while True:
-  for i in range(10):
-    cpx.pixels[(i-9)%10] = (0,0,0)
-    cpx.pixels[(i-6)%10] = (20,20,20)
-    cpx.pixels[(i-3)%10] = (0,0,20)
-    cpx.pixels[i] = (20,0,0)
-    cpx.pixels[(i+1)%10] = (20,0,0)
-    time.sleep(.1)
+    for i in range(10):
+        cpx.pixels[(i - 9) % 10] = (0, 0, 0)
+        cpx.pixels[(i - 6) % 10] = (20, 20, 20)
+        cpx.pixels[(i - 3) % 10] = (0, 0, 20)
+        cpx.pixels[i] = (20, 0, 0)
+        cpx.pixels[(i + 1) % 10] = (20, 0, 0)
+        time.sleep(0.1)
 
+    for i in range[5]:
+        if (i % 2) == 0:
+            cpx.pixels.fill((50, 0, 0))
+        else:
+            cpx.pixels.fill((0, 0, 0))
+
+    cpx.pixels.fill((0, 0, 0))
